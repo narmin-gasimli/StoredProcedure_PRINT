37435ce818672c8e2d158eb5b3f990c449ffbf9c
narmin-gasimli <111438814+narmin-gasimli@users.noreply.github.com>
2025-04-06 14:54:51 +0400

    Stored Procedure and PRINT

---
+++ StoredProcedure_PRINT.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE operations (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    salary DECIMAL(10,2) NOT NULL,
+    date DATE NOT NULL
+);
+
+CREATE PROCEDURE CalculateAndPrintTotal
+AS
+BEGIN
+    -- Məlumat veririk ki, proses başlayıb
+    PRINT 'Hesablama başladı...';
+
+    -- Dəyişən yaradıb məbləği ora yazırıq
+    DECLARE @total DECIMAL(10,2);
+
+    -- Məbləği hesablamaq
+    SELECT @total = SUM(salary) FROM operations;
+
+    -- Nəticəni print edirik
+    PRINT 'Hesablama tamamlandı.';
+    PRINT 'Ümumi məbləğ: ' + CAST(@total AS NVARCHAR(20));
+END;
+
+EXEC CalculateAndPrintTotal;
